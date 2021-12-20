52699eb77f7ee406b3e4a540b6836c25a29dd57c
Daniel Felipe <danypolo18@hotmail.com>
2021-12-20 00:32:05 -0500

    12 commit fixed dependencies of animeDetail

--- src/components/DetailAnime/DetailAnimeScreen.js
+++ src/components/DetailAnime/DetailAnimeScreen.js
@@ -12,7 +12,7 @@ export const DetailAnimeScreen = () => {
         <div className="detail_wrapper">
             <div className="detail_main">
                 <div className="picture">
-                    <img src={picture[0].large ? picture[0].large : `https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg` } alt="anime cover"/>
+                    <img src={picture[0] ? picture[0].large : `https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg` } alt="anime cover"/>
                 </div>
                 <div className="text">
                     <div className="text_title1">
@@ -27,21 +27,25 @@ export const DetailAnimeScreen = () => {
                         icon={ searchOnList ? faHeartbeat : faHeart}/>
                     </div>
                     <div className="text_content">
-                        <h3>{ moreInfo  ? moreInfo : `We are sorry but at the moment we do not have information about this anime`}</h3>
+                        <h3>{ (moreInfo && moreInfo.length > 0)   ? moreInfo : `We are sorry but at the moment we do not have information about this anime`}</h3>
                     </div>
                     <div className="text_character">
                         <div className="text_characters" >
                             <h3>Characters</h3>
                             <div className="avatar-group">
                                 {
-                                    charactersAndStaff.charactersLimit.map((character,idx)=>{
-                                        return(
-                                            <div key={idx} className="avatar ">
-                                                <span className="tooltiptext">{character.name}</span>
-                                                <img src={character.image_url} alt="avatar"/>
-                                            </div>
-                                        )
-                                    })
+                                    charactersAndStaff.charactersLimit
+                                    ?(
+                                        charactersAndStaff.charactersLimit.map((character,idx)=>{
+                                            return(
+                                                <div key={idx} className="avatar ">
+                                                    <span className="tooltiptext">{character.name}</span>
+                                                    <img src={character.image_url} alt="avatar"/>
+                                                </div>
+                                            )
+                                        })
+                                    )
+                                    :(<div className="warning_text">soy fraca</div>)
                                 }
                             </div>
                         </div>
@@ -49,15 +53,18 @@ export const DetailAnimeScreen = () => {
                             <h3>Staff</h3>
                             <div className="avatar-group">
                                 {
-                                    charactersAndStaff.staffLimit.map((staff,idx)=>{
-                                        return(
-                                            <div key={idx} className="avatar">
-                                                <span className="tooltiptext">{staff.name}</span>
-                                                <img src={staff.image_url} alt="avatar"/>
-                                            </div>
+                                    charactersAndStaff.staffLimit
+                                    ?
+                                        charactersAndStaff.staffLimit.map((staff,idx)=>{
+                                            return(
+                                                <div key={idx} className="avatar">
+                                                    <span className="tooltiptext">{staff.name}</span>
+                                                    <img src={staff.image_url} alt="avatar"/>
+                                                </div>
 
-                                        )
-                                    })
+                                            )
+                                        })
+                                    :(<div className="warning_text">soy fraca</div>)
                                 }
                             </div>
                         </div>
@@ -74,16 +81,20 @@ export const DetailAnimeScreen = () => {
                 <h3 className="detail_episodes_title">{nameAndDate.name}<span style={{marginLeft:"8px"}}>Episodes</span></h3>
                 <div className="cards_episodes">
                     {
-                        episodes.map((episode,idx)=>{
-                            return(
-                                <div key={idx} className="card_episode">
-                                    <a style={{textDecoration:"none"}} href={episode.video_url}>
-                                        <img src={picture[1].large} alt="caps"/>
-                                    </a>
-                                        Ep-{episode.episode_id}
-                                </div>
-                            )
-                        })
+                        episodes
+                        ?(
+                            episodes.map((episode,idx)=>{
+                                return(
+                                    <div key={idx} className="card_episode">
+                                        <a style={{textDecoration:"none"}} href={episode.video_url}>
+                                            <img src={picture[0].large} alt="caps"/>
+                                        </a>
+                                            Ep-{episode.episode_id}
+                                    </div>
+                                )
+                            })
+                        )
+                        :(<div className="warning_text">soy fraca</div>)
                     }
                 </div>
             </div>
@@ -91,32 +102,35 @@ export const DetailAnimeScreen = () => {
                 <h3> News of this anime </h3>
                 <div className="detail_new_deck">
                     {
-                        news.map((singleNew,idx)=>{
-                            return(
-                                <div
-                                key={idx}
-                                // href={singleNew.url}
-                                className='deck'>
-                                    <a style={{textDecoration:"none"}} href={singleNew.url}>
-                                        <div className='card-new'>
-                                            <header className='cardHeader'>
-                                                <FontAwesomeIcon style={{color: "#FFD433", margin: "10px"}} icon={faNewspaper}/>
-                                                <span className='cardHeader_account'>@{singleNew.title}</span>
-                                                <span className='cardHeader_date'>{singleNew.date}</span>
-                                            </header>
-                                            <div className='cardBody'>
-                                                <p className='cardText'>{singleNew.intro}</p>
-                                                <section className='cardStats'>
-                                                <span className='cardStats_stat cardStats_stat-comments'>{singleNew.comments}
-                                                <FontAwesomeIcon style={{marginLeft:"4px"}} icon={faComments}/>
-                                                </span>
-                                                </section>
+                        news
+                        ?(
+                            news.map((singleNew,idx)=>{
+                                return(
+                                    <div
+                                    key={idx}
+                                    className='deck'>
+                                        <a style={{textDecoration:"none"}} href={singleNew.url}>
+                                            <div className='card-new'>
+                                                <header className='cardHeader'>
+                                                    <FontAwesomeIcon style={{color: "#FFD433", margin: "10px"}} icon={faNewspaper}/>
+                                                    <span className='cardHeader_account'>@{singleNew.title}</span>
+                                                    <span className='cardHeader_date'>{singleNew.date}</span>
+                                                </header>
+                                                <div className='cardBody'>
+                                                    <p className='cardText'>{singleNew.intro}</p>
+                                                    <section className='cardStats'>
+                                                    <span className='cardStats_stat cardStats_stat-comments'>{singleNew.comments}
+                                                    <FontAwesomeIcon style={{marginLeft:"4px"}} icon={faComments}/>
+                                                    </span>
+                                                    </section>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </a>
-                                </div>
-                            )
-                        })
+                                        </a>
+                                    </div>
+                                )
+                            })
+                        )
+                        :(<div className="warning_text">soy fraca</div>)
                     }
                 </div>
             </div>
